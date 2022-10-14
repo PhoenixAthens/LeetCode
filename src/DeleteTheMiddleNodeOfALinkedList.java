7d1b40bd5d4a70578b38cd4adff0b82e431907ff
Phoenix <anmolkhanna2003@gmail.com>
2022-10-14 21:22:52 +0530

    Daily coding Challenge

---
+++ src/DeleteTheMiddleNodeOfALinkedList.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class DeleteTheMiddleNodeOfALinkedList {
+    private static class ListNode{
+        private int val;
+        private ListNode next;
+        public ListNode(){
+
+        }
+        public ListNode(int val){
+            this.val=val;
+        }
+        public ListNode(int val, ListNode nextNode){
+            this.val=val;
+            this.next=nextNode;
+        }
+
+    }
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode p1=head,p2=head;
+        if (head == null) {
+            return null;
+        }
+        ArrayList<Integer> tempArray=new ArrayList<>();
+        tempArray.add(p1.val);
+        while(p1.next!=null){
+            p1=p1.next;
+            tempArray.add(p1.val);
+        }
+        tempArray.remove((tempArray.size())/2);
+        ListNode ln=new ListNode(),ln2;
+        ln2=ln;
+        if(tempArray.size()==0){
+            return null;
+        }else{
+            for(int i=0;i<tempArray.size();i++){
+                ln2.val=tempArray.get(i);
+                if(i!=tempArray.size()-1){
+                    ln2.next=new ListNode();
+                    ln2=ln2.next;
+                }else{
+                    break;
+                }
+            }
+            return ln;
+        }
+
+
+    }
+
+}
