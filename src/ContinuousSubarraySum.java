1e1eaf8061a7d5b9aba13072abb89c1a22063fb8
Phoenix <anmolkhanna2003@gmail.com>
2022-10-26 18:50:13 +0530

    from 26th

---
+++ src/ContinuousSubarraySum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ContinuousSubarraySum {
+    //this solution exceeds time-limit
+    public static boolean checkSubarraySum(int[] nums, int k) {
+        if(nums.length==1)return false;
+        int i=0;
+        while(i<nums.length-1){
+            int innerSum=nums[i];
+            for(int j=i+1;j<nums.length;j++){
+                innerSum+=nums[j];
+                if( innerSum%k==0) return true;
+            }
+            i++;
+        }
+        return false;
+    }
+    public static void main(String...args){
+        System.out.println(checkSubarraySum(new int[]{23,2,6,2,5},6));
+        System.out.println(0%6);
+    }
+}
