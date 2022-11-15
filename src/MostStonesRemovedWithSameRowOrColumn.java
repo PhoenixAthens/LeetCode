5b83c9b2790b6d07de1d009194e702c3021282cf
Phoenix <anmolkhanna2003@gmail.com>
2022-11-15 19:49:29 +0530

    from November 15th

--- src/MostStonesRemovedWithSameRowOrColumn.java
+++ src/MostStonesRemovedWithSameRowOrColumn.java
@@ -5,57 +5,36 @@ public class MostStonesRemovedWithSameRowOrColumn {
     public static int removeStones(int[][] stones) {
         if(stones.length==1) return 0;
         else{
+            int count=0;
             HashSet<int[]> setOF=new HashSet<>();
-            int counter=0;
-            int k=0;
-            while(k<stones.length){
-                int[] present=stones[k];
-                if(!setOF.contains(present)){
-                    setOF.add(present);
-                    for(int j=0;j<stones.length;j++){
-                        if(!setOF.contains(stones[j])){
-                            if(stones[j][0]==present[0]||stones[j][1]==present[1]){
-                                counter++;
-                                present=stones[j];
-                                j=0;
-                                setOF.add(present);
+            for(int[] i:stones){
+                if(!setOF.contains(i)){
+                    setOF.add(i);
+                    for(int[] j:stones){
+                        if(!setOF.contains(j)){
+                            if(i[0]==j[0] || i[1]==j[1]){
+                                System.out.println(Arrays.toString(i)+" : "+Arrays.toString(j)+" : "+count);
+                                count++;
+                                break;
                             }
                         }
                     }
                 }
-                k++;
             }
-            return counter;
+            return count;
         }
     }
-    /*public static int findMax(int[][] ints){
-        if(ints.length==1) return 0;
-        else{
-            int[] arr=new int[ints.length];
-            for(int i=0;i<arr.length;i++){
-                arr[i]=ints[i][0]*10+ints[i][1];
-            }
-            int k=0;
-            Arrays.parallelSort(arr);
-            int counter=0;
-            for(int i= arr.length-1;i>=0;i--){
-                k=i-1;
-                int present=arr[i];
-                for(;k>=0;k--){
-                    if(arr[k]%10==present%10 || arr[k]/10==present/10){
-                        counter++;
-                        break;
-                    }
-                }
-            }
-            return counter;
-        }
-    }*/
+
     public static void main(String...args){
-        System.out.println(removeStones(new int[][]{{0,1},{1,0},{1,1}}));
+        System.out.println("First: "+removeStones(new int[][]{{0,1},{1,0},{1,1}}));
         //[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]
         //[[0,0],[0,2],[1,1],[2,0],[2,2]]
         int[][] arr=new int[][]{{0,0},{0,2},{1,1},{2,0},{2,2}};
+        int[][] trr=new int[][]{{0,0},{0,1},{1,0},{1,2},{2,1},{2,2}};
+        System.out.println("Second: "+removeStones(arr));
+        System.out.println("Third: "+removeStones(trr));
+        //[[0,1],[1,2],[1,3],[3,3],[2,3],[0,2]] ->this broke the code!!
+
         //System.out.println(findMax(arr));
 
 
