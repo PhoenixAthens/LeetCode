d7cdce487b2da89a15bc22d4edbcde0f5b29cce9
Phoenix <anmolkhanna2003@gmail.com>
2022-11-15 19:21:35 +0530

    from November 15th

---
+++ src/CountCompleteTreeNodes.java
@@ -0,0 +1,44 @@
+import com.sun.source.tree.Tree;
+
+public class CountCompleteTreeNodes {
+    //Time: 0ms, Memory: 45.2MB
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static int countNodes(TreeNode root) {
+        if(root==null){
+            return 0;
+        }else{
+            return recurse(1,root);
+        }
+    }
+    public static int recurse(int count,TreeNode root){
+        if(root.left==null && root.right==null){
+            return count;
+        }else if(root.left!=null && root.right==null){
+            return count+recurse(count,root.left);
+        }else if(root.right!=null && root.left==null){
+            return count+recurse(count,root.right);
+        }else{
+            return count+recurse(count,root.left)+recurse(count,root.right);
+        }
+    }
+
+
+    public static void main(String...args){
+        TreeNode tn=new TreeNode(1,new TreeNode(2,new TreeNode(4),new TreeNode(5)),new TreeNode(3,new TreeNode(6),null));
+        TreeNode gn=null;
+        TreeNode dn=new TreeNode(1,new TreeNode(2),null);
+        System.out.println(countNodes(gn));
+    }
+
+}
