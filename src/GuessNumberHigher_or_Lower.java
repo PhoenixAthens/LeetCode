3fd5ccd1c45ac0bd79e88921abe7e253ad22ffcd
Phoenix <anmolkhanna2003@gmail.com>
2022-11-16 11:49:57 +0530

    from November 16th

---
+++ src/GuessNumberHigher_or_Lower.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class GuessNumberHigher_or_Lower {
+    //works for 21/25 test-cases, need to use BinarySearch to solve this question
+    public int guessNumber(int n){
+        if(guess(n)==-1 || guess(n)==1){
+            for(int i=n;i>=0;i--){
+                if(guess(i)==0)return i;
+            }
+        }else if(guess(n)==1){
+            for(int i=0;i<n;i++){
+                if(guess(i)==0)return i;
+            }
+        }
+        return n;
+    }
+    public int guess(int num){
+        int nome=new Scanner(System.in).nextInt();
+        if(num>nome)return -1;
+        else if(num<nome)return 1;
+        else return 0;
+    }
+}
