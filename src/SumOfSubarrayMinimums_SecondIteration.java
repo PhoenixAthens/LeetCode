5ffa7230c39e3cd93d5365dacd40694b5b0addc8
Phoenix <anmolkhanna2003@gmail.com>
2022-11-29 21:17:03 +0530

    Changees from NOVEMBER 28th

--- src/SumOfSubarrayMinimums_SecondIteration.java
+++ src/SumOfSubarrayMinimums_SecondIteration.java
@@ -1,25 +1,19 @@
 public class SumOfSubarrayMinimums_SecondIteration {
-    //works for 59/87 test cases
-    //fails at: [36,74,84,92,17,68,97,6,68,85]
+    //works for 77/87 test cases
+    //fails at the 77th "Time Limit Exceeded!!"
     public static int sumSubarrayMins(int[] arr) {
         int totalSum=0;
         int bummer=1_000_000_007;
-        int min=0;
+        int min;
         for(int i=0;i<arr.length;i++){
+            min=arr[i];
             for(int j=i;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                    totalSum+=arr[i];
-                    min=arr[i];
-                }else{
-                    min=Math.min(min,arr[j]);
-                    totalSum+=min;
-                }
+                min=Math.min(min,arr[j]);
+                totalSum+=min;
                 totalSum%=bummer;
-                System.out.println("Sum: "+totalSum);
-
             }
         }
-        return totalSum%bummer;
+        return totalSum;
     }
     public static void main(String...args){
         System.out.println(sumSubarrayMins(new int[]{36,74,84,92,17,68,97,6,68,85}));
