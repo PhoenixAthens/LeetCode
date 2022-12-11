35b7f842a1c30fd16dc64a7a994fd144b690e12a
Phoenix <anmolkhanna2003@gmail.com>
2022-12-12 00:58:21 +0530

    from 11th December, still working on it!!

--- src/From_11th_December/BinaryTreeMaximumPathSum_SecondIteration.java
+++ src/From_11th_December/BinaryTreeMaximumPathSum_SecondIteration.java
@@ -1,15 +1,16 @@
 package From_11th_December;
-
-public class BinaryTreeMaximumPathSum {
-    //works for 14/94 test-cases
-    //fails at 15th test-case
+public class BinaryTreeMaximumPathSum_SecondIteration {
+    //works for 31/94 test-cases
+    //fails at 32nd testCase;
     int sum=0;
     public int maxPathSum(TreeNode root) {
+        sum=root.val;
         traverse(root);
         return sum;
     }
     public boolean traverse(TreeNode root){
         if(root.left!=null && root.right!=null){
+            if(root.val>sum)sum=root.val;
             if(traverse(root.left) && traverse(root.right)) {
                 int tempSum = root.val + root.left.val + root.right.val;
                 if(tempSum>sum)
@@ -18,17 +19,18 @@ public class BinaryTreeMaximumPathSum {
 
         }else{
             if(root.right==null && root.left==null){
-                sum+=root.val;
+                if(root.val>sum)sum=root.val;
                 return true;
             }
             if(root.left!=null){
+                if(root.val>sum)sum=root.val;
                 traverse(root.left);
             }
             if(root.right!=null){
+                if(root.val>sum)sum=root.val;
                 traverse(root.right);
             }
         }
         return false;
     }
-
 }
