7d68a74b389352d6f5d1affb25dbb1e1102b6d39
Phoenix <anmolkhanna2003@gmail.com>
2022-12-18 03:19:59 +0530

    from 17th December, can do better!!

---
+++ src/From_17th_December/RansomNote_ThirdIteration.java
@@ -0,0 +1,27 @@
+package From_17th_December;
+import java.util.HashMap;
+public class RansomNote_ThirdIteration {
+    //Runtime: 15ms beats 54.78%, Memory: 78.80MB beats 78.80%
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> note=new HashMap<>();
+        for(int i=0;i<magazine.length();i++){
+            char present=magazine.charAt(i);
+            if(note.containsKey(present)){
+                note.put(present,note.get(present)+1);
+            }else{
+                note.put(present,1);
+            }
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            char pChar=ransomNote.charAt(i);
+            if(note.containsKey(pChar)){
+                int amount=note.get(pChar);
+                if(amount==1)note.remove(pChar);
+                else note.put(pChar,amount-1);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
