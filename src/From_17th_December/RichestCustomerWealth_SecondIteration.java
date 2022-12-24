63cf088b305deba95e19e639c589bd292148c38d
Phoenix <anmolkhanna2003@gmail.com>
2022-12-24 22:02:23 +0530

    from 24th December, Still working on it!!

---
+++ src/From_17th_December/RichestCustomerWealth_SecondIteration.java
@@ -0,0 +1,34 @@
+package From_17th_December;
+import java.util.Arrays;
+public class RichestCustomerWealth_SecondIteration {
+    //Runtime: 2ms, Memory: 41.7MB
+    // Replacing Approach 1 with Approach 2 give:
+    //FI: Runtime: 0ms, Memory: 41.8MB
+    //SI: Runtime: 0ms, Memory: 41.9MB
+    //TI: Runtime: 0ms, Memory: 41.7MB
+    public int maximumWealth(int[][] accounts) {
+        int len=accounts.length;
+        int cols=accounts[0].length;
+        int[] guys=new int[len];
+        for(int i=0;i<cols;i++){
+            int j=0;
+            while(j<len){
+                guys[j]+=accounts[j][i];
+                j++;
+            }
+        }
+        //Approach 1
+        //Arrays.parallelSort(guys);
+        //return guys[len-1];
+
+        //Approach 2
+        if(len==1)return guys[0];
+        else{
+            int max=0;
+            for(int i=0;i<len-1;i+=2){
+                max=Math.max(max,Math.max(guys[i],guys[i+1]));
+            }
+            return max;
+        }
+    }
+}
