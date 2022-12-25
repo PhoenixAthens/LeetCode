d1a7791a869ddc26afac2619aef92549fa2149dc
Phoenix <anmolkhanna2003@gmail.com>
2022-12-26 00:30:18 +0530

    from 25th December, Still working on it!!

--- src/Personal/DecimalToBinary.java
+++ src/Personal/DecimalToBinary.java
@@ -15,17 +15,19 @@ public class DecimalToBinary {
         long num=0;
         long power=0;
         while(binary!=0){
-            num+=(binary%10)*Math.pow(2,power);
-            power++;
+            num+=(binary%10)*Math.pow(2,power++);
             binary/=10;
+            System.out.println(num);
         }
         return num;
     }
     public static void main(String...args){
         System.out.println(new DecimalToBinary().ConvertToBinary(8));
         System.out.println(new DecimalToBinary().ConvertToBinary(9036));
+        System.out.println(new DecimalToBinary().ConvertToBinary(17195));
         System.out.println(new DecimalToBinary().ConvertToDecimal(10001101001100L));
-
+        System.out.println(new DecimalToBinary().ConvertToDecimal(110_0000_0110_0111L));
+        //System.out.println(0234);
         //0010 0011 0100 1100
         //0010 0011 0100 1100
     }
