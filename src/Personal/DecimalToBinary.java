54adc488ee43a3329c5844c3c546e538850e99b7
Phoenix <anmolkhanna2003@gmail.com>
2022-12-26 00:01:43 +0530

    from 25th December, Still working on it!!

--- src/Personal/DecimalToBinary.java
+++ src/Personal/DecimalToBinary.java
@@ -11,12 +11,21 @@ public class DecimalToBinary {
         }
         return num;
     }
-    public long ConvertToDecimal(){
-        return 0;
+    public long ConvertToDecimal(long binary){
+        long num=0;
+        long power=0;
+        while(binary!=0){
+            num+=(binary%10)*Math.pow(2,power);
+            power++;
+            binary/=10;
+        }
+        return num;
     }
     public static void main(String...args){
         System.out.println(new DecimalToBinary().ConvertToBinary(8));
         System.out.println(new DecimalToBinary().ConvertToBinary(9036));
+        System.out.println(new DecimalToBinary().ConvertToDecimal(10001101001100L));
+
         //0010 0011 0100 1100
         //0010 0011 0100 1100
     }
