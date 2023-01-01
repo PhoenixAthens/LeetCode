7cf4b202b7f34d3ec3edfaf822ee4aa6a20f23e7
Phoenix <anmolkhanna2003@gmail.com>
2023-01-01 22:17:47 +0530

    from 1st January,Done deal!!

---
+++ src/JAN_1_2023/WordPattern_ED3.java
@@ -0,0 +1,10 @@
+package JAN_1_2023;
+import java.util.*;
+public class WordPattern_ED3 {
+    // works for 31/37 test cases!!
+    public boolean wordPattern(String pattern, String s) {
+        HashSet<String> make1=new HashSet<>(new ArrayList<>(List.of(pattern.split(""))));
+        HashSet<String> make2=new HashSet<>(new ArrayList<>(List.of(s.split(" "))));
+        return make1.size()==make2.size();
+    }
+}
