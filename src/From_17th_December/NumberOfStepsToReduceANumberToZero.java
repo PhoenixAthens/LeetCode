6654722a49ec7897ba04a5b44fca6dbf6762f5cd
Phoenix <anmolkhanna2003@gmail.com>
2023-01-01 23:28:19 +0530

    from 1st January,Done deal!!

--- src/From_17th_December/NumberOfStepsToReduceANumberToZero.java
+++ src/From_17th_December/NumberOfStepsToReduceANumberToZero.java
@@ -6,6 +6,10 @@ public class NumberOfStepsToReduceANumberToZero {
     //After changes made on 1st of January:
 
     //Runtime:0mx beats 100%, Memory: 38.9MB beats 95.36%
+
+    //The new addition of steps from line 26-29 improves memory consumption!!
+
+    // code at line 14 is unnecessary as well, However it's better to have it in place;
     public int numberOfSteps(int num) {
         if(num==0)return num;
         int count=0;
@@ -19,7 +23,13 @@ public class NumberOfStepsToReduceANumberToZero {
         }
         count++;*/
         while(num!=0){
-           if(num%2==0)num/=2;
+           if(num%2==0){
+               num/=2;
+               if(num%2!=0){
+                   num-=1;
+                   count++;
+               }
+           }
            else num-=1;
            count++;
         }
