73bc21d1d234aaeacbb41e3b64d32e9340034b36
Phoenix <anmolkhanna2003@gmail.com>
2023-01-05 00:26:35 +0530

    from 4th January, Re-Learning some old stuff

--- src/JAN_4_2023/MaxConsecutiveOnes.java
+++ src/JAN_4_2023/MaxConsecutiveOnes.java
@@ -2,17 +2,21 @@ package JAN_4_2023;
 
 public class MaxConsecutiveOnes {
     //Runtime: 5ms beats 40.11%, Memory: 57MB beats 26.49%
+    //
+    // After some improvements
+    //
+    //Runtime: 2ms beats 87.12%, Memory: 57.4MB beats 14.33%
     public int findMaxConsecutiveOnes(int[] nums) {
         int count=0;
         int prevCount=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==1)prevCount++;
-            else{
-                count=Math.max(count,prevCount);
+        for (int num : nums) {
+            if (num == 1) prevCount++;
+            else {
+                if(prevCount>count)count=prevCount;
                 prevCount=0;
             }
         }
-        count=Math.max(count,prevCount);
+        if(prevCount>count)count=prevCount;
         return count;
     }
 }
