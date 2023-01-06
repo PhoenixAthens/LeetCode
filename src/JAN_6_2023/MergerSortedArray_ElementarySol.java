0dc52bebf70620b0c74fe158189ce3f7e5c86cf2
Phoenix <anmolkhanna2003@gmail.com>
2023-01-07 01:29:20 +0530

    from 6th January, SOme changes to old code and moree!!

---
+++ src/JAN_6_2023/MergerSortedArray_ElementarySol.java
@@ -0,0 +1,15 @@
+package JAN_6_2023;
+
+import java.util.Arrays;
+
+public class MergerSortedArray_ElementarySol {
+    //Runtime: 2ms beats 18.80%, Memory: 42.3MB beats 80.61%
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j=0;
+        for(int i=m;i<nums1.length;i++){
+            nums1[i]=nums2[j++];
+        }
+        Arrays.parallelSort(nums1);
+    }
+}
+
