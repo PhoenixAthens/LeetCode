cdc2cd006dbae59b57e6d6d821f8fe83bc2d10fe
Phoenix <anmolkhanna2003@gmail.com>
2023-01-15 17:07:21 +0530

    from 15th January, Height Checker, lousy solution!!

--- src/JAN_15_2023/HeightChecker.java
+++ src/JAN_15_2023/HeightChecker.java
@@ -33,5 +33,33 @@ public class HeightChecker {
     public static void main(String...args){
         new HeightChecker().heightChecker(new int[]{1,1,4,2,1,3});
         new HeightChecker().heightChecker(new int[]{5,1,2,3,4});
+        new HeightChecker().heightChecker(new int[]{2,1,2,1,1,2,2,1});
+        new HeightChecker().heightChecker(new int[]{1,2,2,2,2,2,1,2,2,1});
+        int[] arr=new int[]{1,2,3,4,5};
+        //System.out.println(arr[-1]);
     }
+    /*public int heightChecker(int[] heights) {
+        int[] make2= new int[heights.length];
+        System.arraycopy(heights, 0, make2, 0, heights.length);
+        //int minIndex=0;
+        for(int i=0;i<make2.length-1;i++){
+            if(make2[i+1]<make2[i]){
+                int minIndex=i+1;
+                for(int j=i;j<make2.length;j++){
+                    if(make2[j]<=make2[minIndex])minIndex=j;
+                }
+                int temp=make2[i];
+                make2[i]=make2[minIndex];
+                make2[minIndex]=temp;
+            }
+        }
+        System.out.println(Arrays.toString(make2));
+        int count=0;
+        for(int i=0;i<heights.length;i++){
+            if(make2[i]!=heights[i]){
+                count++;
+            }
+        }
+        return count;
+    }*/
 }
