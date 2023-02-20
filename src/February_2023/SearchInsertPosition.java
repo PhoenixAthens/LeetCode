1e64056aa902924a554bcb3d1e1a035c8e5208fa
Phoenix_Edens <Phoenix.Anmol@gmail.com>
2023-02-21 00:32:13 +0530

    from 20th Feb 2023, Search Insert Position O(log n) solution

---
+++ src/February_2023/SearchInsertPosition.java
@@ -0,0 +1,11 @@
+package February_2023;
+//Date: 20th Feb 2023
+public class SearchInsertPosition {
+    //Runtime:0ms beats 100%, Memory: 42.1MB beats 27.56%
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=target)return i;
+        }
+        return nums.length;
+    }
+}
