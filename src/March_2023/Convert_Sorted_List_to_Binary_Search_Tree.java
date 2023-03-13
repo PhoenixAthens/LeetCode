0073ae81c420e1400983c611b87f93b95b3ecd1a
Phoenix <anmolkhanna2003@gmail.com>
2023-03-13 22:11:12 +0530

    From 13th March, 1ms solution to Convert Sorted List ot Balanced Binary Search tree

--- src/March_2023/Convert_Sorted_List_to_Binary_Search_Tree.java
+++ src/March_2023/Convert_Sorted_List_to_Binary_Search_Tree.java
@@ -1,6 +1,8 @@
 package March_2023;
 import java.util.ArrayList;
+//Date: 13th March 2023
 public class Convert_Sorted_List_to_Binary_Search_Tree {
+    //Runtime: 1ms beats 33.97%, Memory: 43.7MB beats 69.58%
     public class ListNode{
         int val;
         ListNode next;
@@ -24,25 +26,24 @@ public class Convert_Sorted_List_to_Binary_Search_Tree {
         }
     }
     public TreeNode sortedListToBST(ListNode head) {
-        ArrayList<Integer> listOfElements=new ArrayList<>();
+        ArrayList<Integer> fromList=new ArrayList<>();
         while(head!=null){
-            listOfElements.add(head.val);
+            fromList.add(head.val);
             head=head.next;
         }
-        if(listOfElements.size()==0)return null;
-        int rootIndex=(listOfElements.size()-1)/2;
-        int midVal= listOfElements.get(rootIndex);
-        TreeNode tree=new TreeNode(midVal);
-        TreeNode temp=tree;
-        for(int i=rootIndex-1;i>=0;i--){
-            temp.left=new TreeNode(listOfElements.get(i));
-            temp=temp.left;
+        if(fromList.size()==0)return null;
+        else if(fromList.size()==1)return new TreeNode(fromList.get(0));
+        return maker(0,fromList.size(),fromList);
+    }
+    public TreeNode maker(int start, int stop, ArrayList<Integer> arr){
+        int len=stop-start;
+        if(len==0)return null;
+        else{
+            int middle=len/2+start;
+            TreeNode result=new TreeNode(arr.get(middle));
+            result.left=maker(start,middle,arr);
+            result.right=maker(middle+1,stop,arr);
+            return result;
         }
-        temp=tree;
-        for(int i=rootIndex+1;i<listOfElements.size();i++){
-            temp.right=new TreeNode(listOfElements.get(i));
-            temp=temp.right;
-        }
-        return tree;
     }
 }
