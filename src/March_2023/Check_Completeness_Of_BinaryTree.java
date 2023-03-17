8de56ce6d06e1604b3a967ccb0d7d7367c70433a
Phoenix <anmolkhanna2003@gmail.com>
2023-03-18 01:53:06 +0530

    From 18th of March, Check completeness of a binary tree!

---
+++ src/March_2023/Check_Completeness_Of_BinaryTree.java
@@ -0,0 +1,36 @@
+package March_2023;
+import March_2023.Sum_Root_To_Leaf_Numbers_SecondIteration.TreeNode;
+
+import java.util.*;
+//Date: March 18th 2023
+public class Check_Completeness_Of_BinaryTree {
+    //Runtime: 1ms beats 89.96%, Memory: 41.9MB beats 78.1%
+    public boolean isCompleteTree(TreeNode root) {
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        boolean nullNode=false;
+        while(!queue.isEmpty()){
+            TreeNode present=queue.poll();
+            if(present==null)nullNode=true;
+            else{
+                if(nullNode)return false;
+                else{
+                    queue.offer(present.left);
+                    queue.offer(present.right);
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String...args){
+        ArrayDeque<> queue=new ArrayDeque<>();
+        queue.offer(null);
+        queue.offer(null);
+        queue.offer(null);
+        //queue.addLast(null);
+        System.out.println(queue.isEmpty());
+        //System.out.println(queue.poll());
+
+    }
+}
