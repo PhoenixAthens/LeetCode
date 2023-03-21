9d94551b58518162ccf10bcbc115be1b57318508
Phoenix <anmolkhanna2003@gmail.com>
2023-03-22 00:51:58 +0530

    From 22nd March, Number of zero filled subArrays 5ms solution

---
+++ src/March_2023/Number_OF_ZeroFilled_SubArrays_SecondIteration.java
@@ -0,0 +1,28 @@
+package March_2023;
+//Date: 22nd March 2023
+public class Number_OF_ZeroFilled_SubArrays_SecondIteration {
+    //Runtime: 5ms beats 40.75%, Memory: 59.7MB beats 72.41%
+    public long zeroFilledSubarray(int[] nums) {
+        long total=0;
+        long count=0;
+        for(int i:nums){
+            if(i==0){
+                count++;
+            }
+            else if(count>0){
+                long sum=count*(2+count-1)/2;
+                total+=sum;
+                count=0;
+            }
+        }
+        if(count!=0){
+            long sum=count*(2+count-1)/2;
+            total+=sum;
+        }
+        return total;
+    }
+    public static void main(String...args){
+        System.out.println(new Number_OF_ZeroFilled_SubArrays_SecondIteration().zeroFilledSubarray(new int[]{1,3,0,0,2,0,0,4}));
+        System.out.println(new Number_OF_ZeroFilled_SubArrays_SecondIteration().zeroFilledSubarray(new int[]{0,0,0,2,0,0}));
+    }
+}
