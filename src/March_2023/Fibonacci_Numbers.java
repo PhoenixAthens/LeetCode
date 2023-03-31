83c46498704cdfec7c0f0e7c2e48bc6c891a5eda
Phoenix <anmolkhanna2003@gmail.com>
2023-03-31 12:22:06 +0530

    From 27th March, Linear Recursion to find Fibonacci numbers

---
+++ src/March_2023/Fibonacci_Numbers.java
@@ -0,0 +1,19 @@
+package March_2023;
+public class Fibonacci_Numbers {
+    //Runtime: 0ms beats 100%, Memory: 39.7MB beats 21.33%
+    public int fib(int n) {
+        if(n==0)return 0;
+        return LinearFibonacci(n)[0];
+    }
+    public int[] LinearFibonacci(int n){
+        if(n<=1){
+            return new int[]{1,0};
+        }else{
+            int[] calc=LinearFibonacci(n-1);
+            return new int[]{calc[0]+calc[1],calc[0]};
+        }
+    }
+    public static void main(String...args){
+        //System.out.println(fib());
+    }
+}
