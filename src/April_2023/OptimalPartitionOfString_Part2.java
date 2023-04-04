64d6251a096017b974ff88d1aadd08cb88304c2b
Phoenix <anmolkhanna2003@gmail.com>
2023-04-04 11:41:33 +0530

    April 4th 2023, Optimal Partition of String, 9ms solution!

--- src/April_2023/OptimalPartitionOfString_Part2.java
+++ src/April_2023/OptimalPartitionOfString_Part2.java
@@ -9,23 +9,19 @@ public class OptimalPartitionOfString_Part2 {
     //Runtime: 26ms beats 75.36%, Memory: 43.3MB beats 53%
     //Best Score:
     //Runtime: 17ms beats 76.29%, Memory: 43.2MB beats 59.94%
+    //Some further Improvements:
+    //Runtime: 9ms beats 92.55%, Memory: 43.5MB beats 38.41%
     public int partitionString(String s) {
         int[] alphabet= new int[26];
         int count=0;
-        StringBuilder Current=new StringBuilder();
         int strLen=s.length();
         for(int i=0;i<strLen;i++){
             char character=s.charAt(i);
-            if(alphabet[character-97]==0){
-                Current.append(character);
-                alphabet[character-97]+=1;
-            }else{
+            if (alphabet[character - 97] != 0) {
                 count++;
-                alphabet=new int[26];
-                alphabet[character-97]+=1;
-                Current=new StringBuilder();
-                Current.append(character);
+                alphabet = new int[26];
             }
+            alphabet[character-97]+=1;
         }
         return count+1;
     }
