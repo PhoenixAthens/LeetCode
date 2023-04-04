789e1c434923524ea0bf45fcfa6c095b30b8ffb9
Phoenix <anmolkhanna2003@gmail.com>
2023-04-04 11:36:58 +0530

    April 4th 2023, Optimal Partition of String, 18ms solution, looking for a better solution!

--- src/April_2023/OptimalPartitionOfString_Part2.java
+++ src/April_2023/OptimalPartitionOfString_Part2.java
@@ -7,21 +7,23 @@ public class OptimalPartitionOfString_Part2 {
     //Runtime: 18ms beats 76.9%, Memory: 43.4MB beats 38.41%
     //With String Buffer
     //Runtime: 26ms beats 75.36%, Memory: 43.3MB beats 53%
+    //Best Score:
+    //Runtime: 17ms beats 76.29%, Memory: 43.2MB beats 59.94%
     public int partitionString(String s) {
         int[] alphabet= new int[26];
         int count=0;
-        StringBuffer Current=new StringBuffer();
+        StringBuilder Current=new StringBuilder();
         int strLen=s.length();
         for(int i=0;i<strLen;i++){
             char character=s.charAt(i);
-            if(alphabet[122-character]==0){
+            if(alphabet[character-97]==0){
                 Current.append(character);
-                alphabet[122-character]+=1;
+                alphabet[character-97]+=1;
             }else{
                 count++;
                 alphabet=new int[26];
-                alphabet[122-character]+=1;
-                Current=new StringBuffer();
+                alphabet[character-97]+=1;
+                Current=new StringBuilder();
                 Current.append(character);
             }
         }
