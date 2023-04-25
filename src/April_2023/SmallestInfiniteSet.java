c724b66a4ad3d48b34e503fca45566dd7e2ffb56
Phoenix <anmolkhanna2003@gmail.com>
2023-04-25 11:48:31 +0530

    Smallest Number in Infinite Set, 10ms solution

--- src/April_2023/SmallestInfiniteSet.java
+++ src/April_2023/SmallestInfiniteSet.java
@@ -2,21 +2,29 @@ package April_2023;
 //Smallest Number in Infinite Set
 //Date: April 25th 2023
 public class SmallestInfiniteSet {
-    //Runtime: 26ms beats 43.6%, Memory: 43.2MB
+    //Runtime: 24ms beats 43.6%, Memory: 43MB beats 88.67%
+    //After Some Changes
+    //Runtime: 10ms beats 99.43%, Memory: 43.4MB beats 41.36%
     private final int[] set;
+    private int startFrom=1;
     public SmallestInfiniteSet() {
         set = new int[1001];
+
     }
     public int popSmallest() {
-        for(int i=0;i<1001;i++){
+        for(int i=startFrom;i<1001;i++){
             if(set[i]==0){
-                set[i]-=1;
+                set[i]=-1;
+                startFrom=i;
                 return i;
             }
         }
         return -1;
     }
     public void addBack(int num) {
-        if(set[num]<0)set[num]=0;
+        if(set[num]<0){
+            if(num<startFrom)startFrom=num;
+            set[num]=0;
+        }
     }
 }
