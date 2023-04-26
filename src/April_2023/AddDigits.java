96e901a690c6b5de34a7ba05baf94062eb5f835c
Phoenix <anmolkhanna2003@gmail.com>
2023-04-26 07:47:25 +0530

    Add digits, 1ms solution

---
+++ src/April_2023/AddDigits.java
@@ -0,0 +1,19 @@
+package April_2023;
+//Date: April 26th 2023
+public class AddDigits {
+    //Runtime: 1ms beats 100%, Memory: 39.6MB beats 85.75%
+    public int addDigits(int num) {
+        while(num>9){
+            int sum=0;
+            while(num!=0){
+                sum+=num%10;
+                num/=10;
+            }
+            num=sum;
+        }
+        return num;
+    }
+    public static void main(String...args){
+        System.out.println(8%10);
+    }
+}
