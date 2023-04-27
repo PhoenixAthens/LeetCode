63d1549f2a14fc43f74489c79b3785cc20dd831e
Phoenix <anmolkhanna2003@gmail.com>
2023-04-27 10:59:01 +0530

    Bulb Switcher, 0ms solution

---
+++ src/April_2023/BulbSwitcher_NoWay.java
@@ -0,0 +1,8 @@
+package April_2023;
+
+public class BulbSwitcher_NoWay {
+    //Runtime: 0ms beats 100%, Memory: 39.1MB beats 90.46%
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+    }
+}
