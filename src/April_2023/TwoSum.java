d597782d8173889e9abf692ccd544a9dbff5dd5f
Phoenix <anmolkhanna2003@gmail.com>
2023-04-29 14:31:12 +0530

    Two Sum, 1ms solution!

---
+++ src/April_2023/TwoSum.java
@@ -0,0 +1,17 @@
+package April_2023;
+import java.util.HashMap;
+public class TwoSum {
+    //Runtime: 1ms beats 99.32%, Memory: 43MB beats 40.27%
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int remain=target-nums[i];
+            var fromMap=map.get(remain);
+            if(fromMap!=null){
+                return new int[]{i,fromMap};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
