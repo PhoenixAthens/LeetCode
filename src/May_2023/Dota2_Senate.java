953d71e4aec09ed58a139a70a798940c830ab0cb
Phoenix <anmolkhanna2003@gmail.com>
2023-05-04 14:51:48 +0530

    LinkedIN Learning: Java Algorithms Q> Find_Maximum
    LeetCode: Dota2 Senate,57ms solution

--- src/May_2023/Dota2_Senate.java
+++ src/May_2023/Dota2_Senate.java
@@ -4,33 +4,29 @@ public class Dota2_Senate {
     //Attempt1: fails at 74th test case (73/82)
     //A few changes
     //Runtime: 85ms, Memory: 43MB
+    //A few more changes
+    //Runtime: 57ms, Memory: 42.4MB
     public String predictPartyVictory(String senate) {
         char[] main=senate.toCharArray();
-        char victory='Z';
-        while(victory=='Z'){
-
+        while(true){
             for(int i=0;i<main.length;i++){
                 char current=main[i];
                 if(current!='Z'){
-                    int index=-1;
+                    boolean flag=true;
                     char find=(current=='R')?'D':'R';
                     int x=0;
                     for(int j=i+1;j<main.length+i;j++){
                         if(j==main.length)x=main.length;
                         if(main[j-x]==find){
-                            index=j-x;
+                            main[j-x]='Z';
+                            flag=false;
                             break;
                         }
                     }
-                    if(index>-1)main[index]='Z';
-                    else{
-                        victory=current;
-                        break;
-                    }
+                    if(flag) return (current=='R')?"Radiant":"Dire";
                 }
             }
         }
-        return (victory=='R')?"Radiant":"Dire";
     }
     public static void main(String...args){
         System.out.println(new Dota2_Senate().predictPartyVictory("RD"));
