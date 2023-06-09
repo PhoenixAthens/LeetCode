e31a83de61fc8fd72b7ca17c07b5bbdc92fe3f36
Phoenix <anmolkhanna2003@gmail.com>
2023-06-09 15:48:17 +0530

    LinkedIN Learning: Java Algorithms Q> Find_Maximum
    LeetCode: Still working on it!!!
    
    Random: Matrix Multiplication with fewer cache misses!

--- src/May_2023/Design_HashSet.java
+++ src/May_2023/Design_HashSet.java
@@ -1,2 +1,31 @@
-package May_2023;public class Design_HashSet {
+package May_2023;
+
+public class Design_HashSet {
+    class MyHashSet {
+        int[] arrMain;
+        public MyHashSet() {
+            arrMain=new int[256];
+        }
+
+        public void add(int key) {
+            arrMain[calculatePosition(key)]+=1;
+        }
+
+        public void remove(int key) {
+            int post=calculatePosition(key);
+            if(arrMain[post]==key){
+                arrMain[post]=0;
+            }
+        }
+
+        public boolean contains(int key) {
+            return arrMain[calculatePosition(key)] != 0;
+        }
+        public int calculatePosition(int key){
+            int hash = Integer.hashCode(key);
+            int post = hash & (this.arrMain.length-1);
+            if(post<0) post+= arrMain.length;
+            return post;
+        }
+    }
 }
