f739f2a155120730012e19a965f267ca820ea7be
Phoenix <anmolkhanna2003@gmail.com>
2023-10-06 13:04:56 +0530

    LinkedIN Learning: Java Algorithms -> Array Algorithms, getting hands-on with Java Streams. Gotta focus on these topics!
    LeetCode: Majority Element II, 2ms solution!
    
    Random:Implementing Trie

--- src/September_2023/MajorityElement_II.java
+++ src/September_2023/MajorityElement_II.java
@@ -1,6 +1,36 @@
 package September_2023;
 import java.util.*;
 public class MajorityElement_II {
+    //Runtime: 2ms, Memory: 47.03MB
+    public List<Integer> majorityElement_Efficient(int[] nums) {
+        int metric = nums.length/3;
+        int count1 = 0, count2 = 0,candidate1 =0,candidate2= 1;
+        for(int i: nums){
+            if(i==candidate1)count1++;
+            else if(i==candidate2)count2++;
+            else if(count1==0){
+                candidate1 = i;
+                count1++;
+            }else if(count2==0){
+                candidate2 = i;
+                count2++;
+            }else{
+                count1--;
+                count2--;
+            }
+        }
+        ArrayList<Integer> collect = new ArrayList<>();
+        count1 = 0;
+        count2 = 0;
+        for(int n:nums){
+            if(n==candidate1)count1++;
+            else if(n==candidate2)count2++;
+        }
+        if(count1>metric)collect.add(candidate1);
+        if(count2>metric)collect.add(candidate2);
+        return collect;
+    }
+
     //Runtime: 10ms, Memory: 45.96MB
     public List<Integer> majorityElement(int[] nums) {
         int metric = nums.length/3;
