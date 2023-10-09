c4f798d04996e68f78efc2161bcc58da86ac9ad5
Phoenix <anmolkhanna2003@gmail.com>
2023-10-09 15:20:05 +0530

    LinkedIN Learning: Java Algorithms -> Array Algorithms, getting hands-on with Java Streams. Gotta focus on these topics!
    LeetCode: First and last occurrence of target element, 0ms solution using Binary Search
    
    Random:Implementing Trie

--- src/September_2023/First_And_Last_position_Of_Element.java
+++ src/September_2023/First_And_Last_position_Of_Element.java
@@ -16,4 +16,28 @@ public class First_And_Last_position_Of_Element {
             return new int[]{-1,-1};
         }
     }
+    //Runtime: 0ms, Memory: 44.40MB
+    public int[] searchRange_2(int[] nums, int target) {
+        int start = binarySearch(nums,target);
+        if(start==nums.length || nums[start]!=target){
+            return new int[]{-1,-1};
+        }else{
+            int i=start;
+            for(;i<nums.length && nums[i]==target;i++);
+            return new int[]{start,i};
+        }
+    }
+    public int binarySearch(int[] nums, int target){
+        int l = 0;
+        int h = nums.length;
+        while(l < h){
+            int mid = (l + (h-1))/2;
+            if(target <= nums[mid]){
+                h = mid;
+            }else{
+                l = mid+1;
+            }
+        }
+        return l;
+    }
 }
